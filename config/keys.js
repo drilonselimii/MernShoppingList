15f8dad705b32b4a375cba61c2cd7e9a66c300e4
Drilon <drila7@hotmail.com>
2020-07-20 12:15:51 +0200

    Version 1.0.0

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://brad:brad123@cluster0.po0hq.mongodb.net/mern_shopping?retryWrites=true&w=majority'
+};
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const path = require('path');
+
+const items = require('./routes/api/items');
+
+const app = express();
+
+//Bodyparser middleware
+app.use(bodyParser.json());
+
+const db = require('./config/keys').mongoURI;
+
+mongoose
+   .connect(db, { useNewUrlParser: true , useUnifiedTopology: true })
+   .then(() => console.log('MongoDb Connected...'))
+   .catch(err => console.log(err));
+
+   app.use('/api/items', items);
+
+   if(process.env.NODE_ENV === 'production') {
+
+      app.use(express.static('client/build'));
+
+      app.get('*', ( req, res) => {
+         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      });
+
+   }
+
+   const port = process.env.port || 5000;
+
+   app.listen(port, () => console.log(`Server started on port ${port}`));
