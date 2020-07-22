2a074ead2c7169736936b2caa1cfd591eee9046f
Drilon <drila7@hotmail.com>
2020-07-22 13:57:57 +0200

    aa

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ mongoose
       app.use(express.static('client/build'));
 
       app.get('*', ( req, res) => {
-         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+         res.sendFile(path.resolve(__dirname, '/client', '/build', '/index.html'));
       });
 
    }
