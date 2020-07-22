f0fbf2a6f7faa0e45e33f9df653de52d41fe6159
Drilon <drila7@hotmail.com>
2020-07-22 13:41:37 +0200

    aaA

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ mongoose
 
    }
 
-   const port = process.env.port || 5000;
+   const port = process.env.PORT || 5000;
 
    app.listen(port, () => console.log(`Server started on port ${port}`));
