db0b206a4c11997b62b0ab1ef159d187f3ad9b78
Drilon <drila7@hotmail.com>
2020-08-05 18:01:01 +0200

    version 1

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose
 
       app.use(express.static('client/build'));
 
-      app.get('*', ( req, res) => {
+      app.get('*', function( req, res)  {
          res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
       });
 
