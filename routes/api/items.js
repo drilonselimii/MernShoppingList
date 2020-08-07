ab95386806881d936b7ce0b6bdd4665d0c4cef04
Drilon <drila7@hotmail.com>
2020-08-07 15:18:56 +0200

    V2

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 
-const Item = require('../../models/Item');
+const Item = require('../client/models/Item');
 
 router.get('/',(req, res) => {
     Item.find()
